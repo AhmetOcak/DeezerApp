8899241705077ed9ecc2991b9c464875f5e2741c
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-05-11 14:07:04 +0300

    feature modules init

--- feature/designsystem/build.gradle.kts
+++ feature/designsystem/build.gradle.kts
@@ -1,31 +1,27 @@
-import com.android.build.api.dsl.Packaging
-
 plugins {
-    id("com.android.application")
+    id("com.android.library")
     id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace = "com.deezerapp"
+    namespace = "com.designsystem"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.deezerapp"
         minSdk = 26
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
@@ -38,19 +34,14 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.6"
-    }
-    fun Packaging.() {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
 }
 
 dependencies {
 
     implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
     implementation("androidx.activity:activity-compose:1.5.1")
     implementation(platform("androidx.compose:compose-bom:2023.04.01"))
@@ -58,15 +49,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2022.10.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-
-    implementation(project(":feature:musicgenres"))
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,3 +14,11 @@ dependencyResolutionManagement {
 }
 rootProject.name = "DeezerApp"
 include(":app")
+include(":feature")
+include(":feature:musicgenres")
+include(":feature:designsystem")
+include(":feature:ui")
+include(":feature:favorites")
+include(":feature:artists")
+include(":feature:albumdetail")
+include(":feature:artistdetail")
