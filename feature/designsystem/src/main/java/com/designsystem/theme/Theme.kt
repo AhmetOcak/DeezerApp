1b4094e3fe9f56e89f7b4e220c9679c5084fe704
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-05-13 00:01:21 +0300

    theme set

--- feature/designsystem/src/main/java/com/designsystem/theme/Theme.kt
+++ feature/designsystem/src/main/java/com/designsystem/theme/Theme.kt
@@ -6,19 +6,22 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
     tertiary = Pink80,
-    outlineVariant = Purple80
+    outlineVariant = Purple80,
+    onSecondaryContainer = Color.White
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    onSecondaryContainer = Color.Black
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
