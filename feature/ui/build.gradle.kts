93a2726c886f9456432755965ee1262e2d01331a
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-05-14 16:07:53 +0300

    gif component added

--- feature/ui/build.gradle.kts
+++ feature/ui/build.gradle.kts
@@ -57,5 +57,9 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.1.0")
+    implementation("io.coil-kt:coil-gif:2.3.0")
+
     implementation(project(":feature:designsystem"))
 }
---
+++ feature/ui/src/main/java/com/ui/Gif.kt
@@ -0,0 +1,41 @@
+package com.ui
+
+import android.content.Context
+import android.os.Build.VERSION.SDK_INT
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.ImageLoader
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.request.ImageRequest
+
+private val GIF_HEIGHT = 32.dp
+
+@Composable
+fun Gif(modifier: Modifier = Modifier, context: Context) {
+    val imageLoader = ImageLoader.Builder(context)
+        .components {
+            if (SDK_INT >= 28) {
+                add(ImageDecoderDecoder.Factory())
+            } else {
+                add(GifDecoder.Factory())
+            }
+        }.build()
+
+    Image(
+        modifier = modifier.fillMaxWidth().height(GIF_HEIGHT),
+        painter = rememberAsyncImagePainter(
+            model = ImageRequest.Builder(context).data(R.drawable.playing),
+            imageLoader = imageLoader
+        ),
+        contentScale = ContentScale.Crop,
+        contentDescription =null
+    )
+
+}
