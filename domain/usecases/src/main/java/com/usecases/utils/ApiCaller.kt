fd6add9261addfe095c84061a0d14fdfd1175107
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2024-02-26 00:05:16 +0300

    logs removed from callers

--- domain/usecases/src/main/java/com/usecases/utils/ApiCaller.kt
+++ domain/usecases/src/main/java/com/usecases/utils/ApiCaller.kt
@@ -1,6 +1,5 @@
 package com.usecases.utils
 
-import android.util.Log
 import com.usecases.R
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -10,10 +9,8 @@ suspend inline fun <T> apiCall(crossinline call: suspend () -> T): Flow<Response
     try {
         emit(Response.Success(data = call()))
     } catch (e: IOException) {
-        Log.e("API CALL EXCEPTION", e.stackTraceToString())
         emit(Response.Error(errorMessageId = R.string.network_error))
     } catch (e: Exception) {
-        Log.e("API CALL EXCEPTION", e.stackTraceToString())
         emit(Response.Error(errorMessageId = R.string.unknown_error))
     }
 }
--- domain/usecases/src/main/java/com/usecases/utils/DbCaller.kt
+++ domain/usecases/src/main/java/com/usecases/utils/DbCaller.kt
@@ -1,6 +1,5 @@
 package com.usecases.utils
 
-import android.util.Log
 import com.usecases.R
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -9,7 +8,6 @@ suspend inline fun <T> dbCall(crossinline call: suspend () -> T): Flow<Response<
     try {
         emit(Response.Success(data = call()))
     } catch (e: Exception) {
-        Log.e("DB CALL EXCEPTION", e.stackTraceToString())
         emit(Response.Error(errorMessageId = R.string.unknown_error))
     }
 }
